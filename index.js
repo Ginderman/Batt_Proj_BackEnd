b8cd707a616eb833763cd0fa9326f229ed3f433e
Eric Ginder <ginderman10@gmail.com>
2024-06-09 05:03:24 -0700

    Added bodyparser and a second route to handle incoming axios requests

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 const app = express()
 const cors = require('cors');
 
 // Middleware
 app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
 
 
 
+// app.get('/', function (req, res) {
+//     res.send('Hello World')
+// })
 
 
+app.post('/', function (req, res) {
+    console.log(req.body)
+    res.send('Hit Post Route')
+})
 
-app.get('/', function (req, res) {
-    res.send('Hello World')
-  })
+app.post('/:MechID', function (req, res) {
+    
+    const { MechID } = req.params;
+    console.log(MechID) 
+
+    console.log(req.body);
+    res.send('Hit Params Route');
+})
 
 app.listen(3050, () => {
     console.log("Listening on port 3050!");
