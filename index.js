a7ea4fefc77b48ccae69bbac08b4f542f9efe87f
Ginderman <ginderman10@gmail.com>
2024-07-28 20:34:47 -0700

    Update index.js
    Removed cookie logic until further review.

--- index.js
+++ index.js
@@ -14,30 +14,30 @@ const app = express()
 app.use(cors( {credentials: true, origin: 'http://localhost:5173'}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }))
-app.use(session({
-    name: 'TestCookie',
-    secret: "to-be-changed",
-    resave: false,
-    saveUninitialized: true,
-    cookie : {
+// app.use(session({
+//     name: 'TestCookie',
+//     secret: "to-be-changed",
+//     resave: false,
+//     saveUninitialized: true,
+//     cookie : {
             
-            httpOnly: true,
-            maxAge: 24 * 60 * 60 * 1000, // 1 day
+//             httpOnly: true,
+//             maxAge: 24 * 60 * 60 * 1000, // 1 day
             
-    }
+//     }
     
-}));
+// }));
 
 
 
 
-function checkCookie (req,res,next){
-    console.log('CheckCookie Middleware!');
-    console.log('Session ID:', req.sessionID);
-    console.log('Session:', req.session);
-    console.log(req.session.user);
-    next();
-}
+// function checkCookie (req,res,next){
+//     console.log('CheckCookie Middleware!');
+//     console.log('Session ID:', req.sessionID);
+//     console.log('Session:', req.session);
+//     console.log(req.session.user);
+//     next();
+// }
 
 
 
